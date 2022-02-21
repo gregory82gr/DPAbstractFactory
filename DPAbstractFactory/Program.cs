0b738939e825962db896b284deb5dde71359c6a6
Agathangelidis Grigorios <gregory82gr@gmail.com>
2022-02-21 20:57:47 +0200

    input from console
    input from console

--- DPAbstractFactory/Program.cs
+++ DPAbstractFactory/Program.cs
@@ -9,19 +9,23 @@ namespace DPAbstractFactory
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Wooden Factory");
-            IFurnitureFactory woodenFurnitureFactory = new WoodenFurnitureFactory();
-            FurnitureClient clientW = new FurnitureClient(woodenFurnitureFactory);
-
-            Console.WriteLine("Plastic Factory");
-            IFurnitureFactory plasticFurnitureFactory = new PlasticFurnitureFactory();
-            FurnitureClient clientP = new FurnitureClient(plasticFurnitureFactory);
-
-            Console.WriteLine("Metal Factory");
-            IFurnitureFactory metalFurnitureFactory = new MetalFurnitureFactory();
-            FurnitureClient clientM = new FurnitureClient(metalFurnitureFactory);
-            // Wait for user input
+            Console.Write("Enter W,P or M: ");
+            string val = Console.ReadLine();
+            if (val == "W")
+            {
+                Console.WriteLine("Wooden Factory");
+                IFurnitureFactory woodenFurnitureFactory = new WoodenFurnitureFactory();
+                FurnitureClient clientW = new FurnitureClient(woodenFurnitureFactory);
+            } else if (val == "P") {
+                Console.WriteLine("Plastic Factory");
+                IFurnitureFactory plasticFurnitureFactory = new PlasticFurnitureFactory();
+                FurnitureClient clientP = new FurnitureClient(plasticFurnitureFactory);
+            } else if (val == "M") { 
+                Console.WriteLine("Metal Factory");
+                IFurnitureFactory metalFurnitureFactory = new MetalFurnitureFactory();
+                FurnitureClient clientM = new FurnitureClient(metalFurnitureFactory);
+            }
+        // Wait for user input
             Console.ReadKey();
         }
     }
